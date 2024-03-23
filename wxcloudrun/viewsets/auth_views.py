a5feb4d83ce16d7f624b379c0bdc6f10227477d9
gongfeng <981544717@qq.com>
2024-03-23 16:36:12 +0800

    auth接口验证用户权限

--- wxcloudrun/viewsets/auth_views.py
+++ wxcloudrun/viewsets/auth_views.py
@@ -5,6 +5,8 @@ import os
 import requests
 from django.http import JsonResponse
 
+from wxcloudrun.models import AuthUser, Counters
+
 logger = logging.getLogger('log')
 
 wx_api_backend = os.environ["WX_API_BACKEND"]
@@ -20,15 +22,21 @@ def get_auth(request):
 
      `` request `` 请求对象
     """
-    body = json.loads(decode_request_data(request))
-    logging.error(body)
-    if 'token' not in body:
+    try:
+        body = json.loads(decode_request_data(request))
+    except Exception as e:
+        logger.error("解析请求body失败")
+        return JsonResponse({'code': 0, 'data': '解析请求body失败'})
+    if 'code' not in body:
         return JsonResponse({'code': 0, 'data': '无token'})
-    token = body['token']
+    code = body['code']
     res = requests.get(f'{wx_api_backend}/sns/jscode2session?appid={app_id}&'
-                       f'secret={app_secret}&js_code={token}&grant_type=authorization_code')
+                       f'secret={app_secret}&js_code={code}&grant_type=authorization_code')
     if res.status_code != 200:
         logger.error(f'获取用户信息失败:{res.text}')
         return JsonResponse({'code': 0, 'data': '用户信息请求失败'})
-    logging.error(res)
-    return JsonResponse({'code': 1, 'data': '请求成功'})
+    open_id = res.json()['open_id']
+    if AuthUser.objects.filter(open_id=open_id).exists():
+        return JsonResponse({'code': 1, 'data': '请求成功'})
+    else:
+        return JsonResponse({'code': 0, 'data': '非admin用户'})
